8e4c2c5a14cc6e29751e7c7a6cc051f9d2489300
Andrei Roberto Oleniuc <andrei.oleniuc@apiax.com>
2023-11-12 16:56:58 +0100

    initial commit for project

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is my second attempt at React Native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+  }
+});
---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Cal Counter
+
+The goal of this mobile app is to allow a User to track their Nurtition Values and Calories to achieve a better lifestyle.
+
+User can selects goals such as maintaining weight, gaining or loosing. They can then add foods to their day by either searching for food or scanning barcodes. This allows for a simple user experience
+
+This Project is based on React Native with Expo. It uses both the OpenFoodFacts API as well as FoodData Central API.
